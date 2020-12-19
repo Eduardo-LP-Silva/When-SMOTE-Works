c888ab063998faef0a462364e4f994a43f947e01
Eduardo Silva <eduardo__lps@hotmail.com>
2020-12-19 23:02:19 +0000

    Add meta feature calculation and minimum number of classes validation

--- wsw.py
+++ wsw.py
@@ -18,8 +18,9 @@ RESULTS_PATH = './results.csv'
 RESULTS_BACKUP_PATH = './results_backup.csv'
 MISSING_VALUES_THRESHOLD = 10
 SMOTE_VARIANTS = [SMOTE] # TODO Complete
+MIN_MINORITY_NO = 10
 UPSAMPLE_RATIOS = [0.3, 0.6, 0.8, 1.0]
-RESULTS_COLS = ['DATASET', 'AUC-IMB', 'SMOTE-VARIANT', 'UPSAMPLE-RATIO', 'AUC-BLC'] #TODO Add metafeatures
+RESULTS_COLS = ['DATASET', 'SMOTE-VARIANT', 'UPSAMPLE-RATIO', 'AUC-IMB', 'AUC-BLC'] #TODO Add metafeatures
 
 # Returns a model's AUC
 def calc_auc(clf, x_test, y_test):
@@ -54,16 +55,30 @@ def calc_minority_ratio(y):
     return class_counts.min() / class_counts.max()
 
 def calc_mf(x, y):
-    mfe = MFE(groups=['general', 'statistical', 'info-theory', 'clustering', 'complexity'], random_state=42)
-    mfe.fit(x, y)
-    ft = mfe.extract()
+    mfe = MFE(groups=['general', 'statistical', 'info-theory', 'model-based', 'landmarking', 'clustering', 'concept', 
+        'complexity'], random_state=42)
+    mfe.fit(x, y, suppress_warnings=True)
+    ft = mfe.extract(suppress_warnings=True)
+    #print("\n".join("{:50} {:30}".format(x, y) for x, y in zip(ft[0], ft[1])))
     return ft
 
 def multiclass_to_binary(y):
     classes = y.value_counts()
 
     if len(y.index) > 2:
+        majority_no = classes.max()
+
+        if majority_no < MIN_MINORITY_NO:
+            raise Exception('Max number of examples of a class in Y is %d, must be >= %d' % (majority_no, MIN_MINORITY_NO))
+
+        minority_no = classes.min()
         minority_class = classes.idxmin()
+
+        while minority_no < MIN_MINORITY_NO:
+            classes.drop(minority_class, inplace=True)
+            minority_no = classes.min()
+            minority_class = classes.idxmin()
+
         y = y.transform(lambda x: 1 if x == minority_class else 0)
 
     return y
@@ -111,22 +126,32 @@ def load_data(dataset_path):
 def run_experiment(results_writer, dataset_file_name, test_set_ratio=0.2):
         x, y = load_data(os.path.join(DATA_DIR_PATH, dataset_file_name))
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_set_ratio, stratify=y, random_state=42)
-        calc_mf(x_train.values, y_train.values)
+        mf = calc_mf(x_train.values, y_train.values)
         clf = train_random_forest(x_train, y_train)
         auc_imb = '{auc:.3f}'.format(auc=test(clf, x_test, y_test))
     
         for i in range(len(SMOTE_VARIANTS)):
+            print('--> Applying %s' % SMOTE_VARIANTS[i].__name__)
             minority_ratio = calc_minority_ratio(y_train)
             
             for upsample_ratio in UPSAMPLE_RATIOS:
                 if minority_ratio < upsample_ratio:
+                    print('---> %.1f Upsample Ratio' % upsample_ratio)
                     smote_variant = SMOTE_VARIANTS[i](sampling_strategy=upsample_ratio, k_neighbors=3, random_state=42)
                     x_train_upsampled, y_train_upsampled = smote_variant.fit_resample(x_train.values, y_train.values)
                     
                     clf = train_random_forest(x_train_upsampled, y_train_upsampled)
                     auc_smote = '{auc:.3f}'.format(auc=test(clf, x_test, y_test))
-                    #TODO Add metafeatures
-                    results_writer.writerow([dataset_file_name, auc_imb, SMOTE_VARIANTS[i].__name__, upsample_ratio, auc_smote])
+                    mf_smote = calc_mf(x_train_upsampled, y_train_upsampled)
+                    original_mf = mf[1].copy()
+                    
+                    for j in range(len(mf_smote[1])):
+                        original_mf[j] = '%.3f|%.3f' % (float(mf[1][j]), float(mf_smote[1][j]))
+
+                    results_row = [dataset_file_name, SMOTE_VARIANTS[i].__name__, upsample_ratio, auc_imb, auc_smote]
+                    results_row = results_row + original_mf
+
+                    results_writer.writerow(results_row)
 
 def main():
     config = ConfigParser()
@@ -151,7 +176,12 @@ def main():
         results_writer = csv.writer(results_file, delimiter=',')
 
         if results_file.tell() == 0:
-            results_writer.writerow(RESULTS_COLS)
+            rand_x = np.random.randint(1, 100, size=(50, 4))
+            rand_y = np.random.randint(0, 2, size=(50, 1))
+            mf = calc_mf(rand_x, rand_y)
+            header = RESULTS_COLS.copy()
+            header = header + mf[0]
+            results_writer.writerow(header)
 
         data_dir = os.fsencode(DATA_DIR_PATH)
         data_dir_files = os.listdir(data_dir)
